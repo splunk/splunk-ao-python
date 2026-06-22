09f446cd5ab95e0ee87268ab1ed7cc7137bba843
shuningc <shuningc@splunk.com>
2026-06-22 12:05:12 -0700

    Removing GalileoScorers

--- src/splunk_ao/schema/metrics.py
+++ src/splunk_ao/schema/metrics.py
@@ -1,5 +1,4 @@
-import warnings
-from collections.abc import Callable, Iterator
+from collections.abc import Callable
 from enum import Enum
 from typing import Any, Generic, TypeVar
 
@@ -10,7 +9,6 @@ from galileo_core.schemas.logging.span import Span
 from galileo_core.schemas.logging.step import STEP_TYPES_WITH_CHILD_SPANS, StepType
 from galileo_core.schemas.logging.trace import Trace
 from galileo_core.schemas.shared.metric import MetricValueType
-from galileo_core.schemas.shared.scorers.scorer_name import ScorerName
 
 
 class SplunkAOMetrics(str, Enum):
@@ -69,55 +67,6 @@ class SplunkAOMetrics(str, Enum):
     tool_selection_quality = "Tool Selection Quality"
     tool_selection_quality_luna = "Tool Selection Quality (SLM)"
     user_intent_change = "User Intent Change"
-
-
-class _SplunkAOScorersProxyMeta(type):
-    """Metaclass that makes GalileoScorers a deprecated proxy for ScorerName."""
-
-    _DEPRECATION_MSG = (
-        "GalileoScorers is deprecated and will be removed in a future release. Use galileo_core ScorerName instead."
-    )
-
-    def __getattribute__(cls, name: str) -> Any:
-        if name.startswith("_") or name in ("__class__", "__mro__", "__dict__", "__bases__"):
-            return type.__getattribute__(cls, name)
-        warnings.warn(cls._DEPRECATION_MSG, DeprecationWarning, stacklevel=2)
-        return getattr(ScorerName, name)
-
-    def __getitem__(cls, name: str) -> ScorerName:
-        warnings.warn(cls._DEPRECATION_MSG, DeprecationWarning, stacklevel=2)
-        return ScorerName[name]
-
-    def __instancecheck__(cls, instance: Any) -> bool:
-        return isinstance(instance, ScorerName)
-
-    def __subclasscheck__(cls, subclass: type) -> bool:
-        return issubclass(subclass, ScorerName)
-
-    def __iter__(cls) -> Iterator[ScorerName]:
-        warnings.warn(cls._DEPRECATION_MSG, DeprecationWarning, stacklevel=2)
-        return iter(ScorerName)
-
-    def __contains__(cls, item: Any) -> bool:
-        warnings.warn(cls._DEPRECATION_MSG, DeprecationWarning, stacklevel=2)
-        return item in ScorerName
-
-    def __dir__(cls) -> list[str]:
-        return [m.name for m in ScorerName]
-
-    def __repr__(cls) -> str:
-        return "<deprecated GalileoScorers proxy - use ScorerName>"
-
-
-class _SplunkAOScorersProxy(metaclass=_SplunkAOScorersProxyMeta):
-    """Deprecated proxy that forwards to ScorerName."""
-
-    def __new__(cls, value: Any) -> ScorerName:
-        warnings.warn(_SplunkAOScorersProxyMeta._DEPRECATION_MSG, DeprecationWarning, stacklevel=2)
-        return ScorerName(value)
-
-
-GalileoScorers = _SplunkAOScorersProxy
 
 
 MetricType = TypeVar("MetricType", bound=MetricValueType)
